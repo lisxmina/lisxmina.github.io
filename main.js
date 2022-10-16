62c5959f04379566c5685bdac734355d5964806f
Lisa <lisalchmail@gmail.com>
2022-10-16 16:07:27 +0200

    essai 16

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ nextBtn.addEventListener("click", () => {
   index = (index < 2 - 1) ? index + 1 : 1
 document.querySelector(".control .selected").classList.remove("selected")
 indicatorParent.children[index].classList.add("selected")
-slider.style.transform = "translateX(" + (index) * 100 + "%)"
+slider.style.transform = "translateX(" + (index) * 200 + "%)"
 })
 
 
